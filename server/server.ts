cfae782c2ab29d4be514cfa9713d2c1865b79386
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-09-08 19:02:57 +0300

    feat: add upload

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,10 @@
 import {Response, Request, NextFunction} from "express";
 const multer=require('multer')
+const upload=multer({dest:'upload'})
 const express = require('express');
 let bodyParser = require('body-parser');
 import {getHandler} from "./get/get";
-
+let fs=require('fs')
 let path = require('path')
 let postHandler = require("./post/post");
 const logger = require('./logger/logger');
@@ -15,22 +16,42 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     next()
 
 });
+
+
+
+
 path = '/Users/pm/Desktop/Astra/projects/module2/part3/Gallery/client/';
 app.use(bodyParser.json())
 app.use(fileUpload({}));
 app.use(express.static(path))
-app.use(multer({dest:'upload'}).single('filedata'));
+//app.use(multer({dest:'upload'}));
 app.get('', (req: Request, res: Response) => {
     res.sendFile(path)
 })
-app.get('/gallery', (request: Request, response: Response) => {
+app.get('/gallery', upload.any(),(request: Request, response: Response) => {
     let result = getHandler(request)
 
     response.send(result)
 })
 
-app.post('/gallery', (req: Request, res: Response) => {
-})
+
+// Костыль для req.file потом исправить
+
+
+    app.post('/',(req: any, res: Response) => {
+        let filedata = req.files.filedata;
+
+        if(filedata){
+            fs.writeFile(`./img/${filedata.name}`,filedata.data,()=>{
+                console.log('ok')
+            })
+        }
+        else{
+            console.log('err upload')
+        }
+
+
+    })
 
 //Catch post request
 app.post('/auth', (request: Request, response: Response) => {
